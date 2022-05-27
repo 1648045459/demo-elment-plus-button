09358334a6dc785b2d2effee9015d58a7b598f43
hu <hu_fl@hisuntech.com>
2022-05-27 16:50:12 +0800

    ccc

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -8,15 +8,4 @@
 <script lang="ts" setup>
 </script>
 <style scoped>
-.vercode-info {
-  width: 100%;
-  color: #67c23a;
-  text-align: center;
-}
-.password-input :deep(.el-input--suffix .el-input__inner) {
-  padding-right: 0px;
-}
-.password-input :deep(.el-input__suffix) {
-  display: none;
-}
 </style>
